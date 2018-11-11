4274a5c534fbfbaf40546f6bd40ec2ceb41fc505
hecjara <hec.jara@alumnos.duoc.cl>
2018-11-11 19:30:07 -0300

    se agrega el agregar perro con json

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import listar_perros
+from .views import listar_perros, agregar_perro
 urlpatterns = [
-    path('perros/', listar_perros, name="api_listar_perros")
+    path('perros/', listar_perros, name="api_listar_perros"),
+    path('perros/agregar/', agregar_perro, name="api_agregar_perro"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.core import serializers
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 import json
 from core.models import PerroFundacion, Genero, Raza, Estado
 # Create your views here.
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+
 
 def listar_perros(request):
     perros = PerroFundacion.objects.all()
@@ -12,4 +15,29 @@ def listar_perros(request):
 
     return HttpResponse(perrosJson, content_type="application/json")
 
+@csrf_exempt
+@require_http_methods(['POST'])
+def agregar_perro(request):
+    body = request.body.decode('utf-8')
 
+    body_diccionario = json.loads(body)
+
+    perro = PerroFundacion()
+    perro.nombre = body_diccionario['nombre']
+    perro.raza = Raza(id=body_diccionario['raza_id'])
+    perro.genero = Genero(id=body_diccionario['genero_id'])
+    perro.fechaIngreso = body_diccionario['fechaIngreso']
+    perro.fechaNacimiento = body_diccionario['fechaNacimiento']
+    perro.estado = Estado(id=body_diccionario['estado_id'])
+
+    try:
+        perro.save()
+        mensaje = {
+            'mensaje':'guardado correctamente'
+        }
+        return HttpResponse(json.dumps(mensaje), content_type="application/json")
+    except:
+        mensaje = {
+            'mensaje':'error al guardar'
+        }
+        return HttpResponseBadRequest(json.dumps(mensaje), content_type="application/json")
